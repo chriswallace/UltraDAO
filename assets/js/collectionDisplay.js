2723404ff8648d2013702908022e570f0d745345
Chris Wallace <chriswallace7@protonmail.com>
2023-04-26 13:44:23 -0500

    fixed artwork grid display

--- assets/js/collectionDisplay.js
+++ assets/js/collectionDisplay.js
@@ -1,72 +1,70 @@
 function hitApi(url, callback) {
-    var req = new XMLHttpRequest();
-  
-    req.addEventListener('load', onLoad);
-    req.addEventListener('error', onFail);
-    req.addEventListener('abort', onFail);
-  
-    req.open('GET', url);
-    req.send();
-  
-    function onLoad(event) {
-      if (req.status >= 400) {
-        onFail(event);
-      } else {
-        var json = JSON.parse(this.responseText);
-        callback(null, json);
-      }
-    }
-  
-    function onFail(event) {
-      callback(new Error('...'));
+  var req = new XMLHttpRequest();
+
+  req.addEventListener("load", onLoad);
+  req.addEventListener("error", onFail);
+  req.addEventListener("abort", onFail);
+
+  req.open("GET", url);
+  req.send();
+
+  function onLoad(event) {
+    if (req.status >= 400) {
+      onFail(event);
+    } else {
+      var json = JSON.parse(this.responseText);
+      callback(null, json);
     }
   }
 
-document.addEventListener("DOMContentLoaded", function() {
-var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
-
-if ("IntersectionObserver" in window) {
-    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-        let lazyImage = entry.target;
-        lazyImage.src = lazyImage.dataset.src;
-        lazyImage.srcset = lazyImage.dataset.srcset;
-        lazyImage.classList.remove("lazy");
-        lazyImageObserver.unobserve(lazyImage);
-        }
-    });
-    });
-
-    lazyImages.forEach(function(lazyImage) {
-    lazyImageObserver.observe(lazyImage);
-    });
-} else {
-    // Possibly fall back to event handlers here
+  function onFail(event) {
+    callback(new Error("..."));
+  }
 }
-});
 
-CollectionDisplay = (function() {
+function initLazyLoad() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
 
-  var container = document.getElementById('collection-display');
-
-  console.log(container);
-
-  if( container ){
-
-    test = hitApi("/isles.json", function(test, collection){
-
-        collection.data.forEach(obj => {
-
-            htmlString =  `<div class="grid-item">`;
-            htmlString += `<img src="/assets/images/curated-1-hero.jpg" data-src="https://media-cdn.deca.art/ethereum/0xA708AbaBBF9cB9707fF352CF954767b3B5E6a216/${obj.tokenId}/original.png?tx=w_420,c_limit" data-srcset="https://media-cdn.deca.art/ethereum/0xA708AbaBBF9cB9707fF352CF954767b3B5E6a216/${obj.tokenId}/original.png?tx=w_840,c_limit 2x, https://media-cdn.deca.art/ethereum/0xA708AbaBBF9cB9707fF352CF954767b3B5E6a216/${obj.tokenId}/original.png?tx=w_420,c_limit 1x" class="lazy" alt="">`;
-            htmlString += `<h3>#${obj.data.name}</h3>`;
-            htmlString += `</div>`;
-            container.innerHTML += htmlString;
-        });
-
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
     });
 
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Possibly fall back to event handlers here
   }
+}
 
+CollectionDisplay = (function () {
+  var container = document.getElementById("collection-display");
+
+  if (container) {
+    hitApi("/isles.json", function (test, collection) {
+      let index = 0;
+      collection.data.forEach((obj) => {
+        index++;
+        htmlString = `<div class="grid-item"><a href="https://opensea.io/assets/ethereum/0xa708ababbf9cb9707ff352cf954767b3b5e6a216/${index}">`;
+        htmlString += `<img src="/assets/images/curated-1-hero.jpg" data-src="https://media-cdn.deca.art/ethereum/0xA708AbaBBF9cB9707fF352CF954767b3B5E6a216/${obj.tokenId}/original.png?tx=w_420,c_limit" data-srcset="https://media-cdn.deca.art/ethereum/0xA708AbaBBF9cB9707fF352CF954767b3B5E6a216/${obj.tokenId}/original.png?tx=w_840,c_limit 2x, https://media-cdn.deca.art/ethereum/0xA708AbaBBF9cB9707fF352CF954767b3B5E6a216/${obj.tokenId}/original.png?tx=w_420,c_limit 1x" class="lazy" alt="">`;
+        htmlString += `<h3>Isles upon Deemed Rivers #${obj.data.name}</h3>`;
+        htmlString += `</a></div>`;
+        container.innerHTML += htmlString;
+      });
+      // Initialize lazy loading after images have been added to the DOM
+      initLazyLoad();
+    });
+  }
 })();
